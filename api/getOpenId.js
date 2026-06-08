7d6cba40d4bb7b450b9268dcb8716190117afdb7
yaoyunfeng <starcpdk@163.com>
2026-06-08 21:45:39 +0800

    提交

--- api/getOpenId.js
+++ api/getOpenId.js
@@ -1,16 +1,14 @@
 /**
  * Vercel Serverless Function - 微信小程序 code 换 openid
- * 部署到 Vercel 后，小程序调用此接口获取真实 openid
  *
- * 使用方式：
- * GET /api/getOpenId?code=xxx
+ * 使用方式：GET /api/getOpenId?code=xxx
  *
  * 环境变量（在 Vercel 项目设置中配置）：
  * - WX_APPID: 微信小程序 AppID
  * - WX_SECRET: 微信小程序 AppSecret
  */
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   // 只允许 GET 请求
   if (req.method !== 'GET') {
     return res.status(405).json({ error: 'Method not allowed' });
@@ -30,11 +28,20 @@ export default async function handler(req, res) {
   }
 
   try {
-    const wxRes = await fetch(
-      `https://api.weixin.qq.com/sns/jscode2session?appid=${appId}&secret=${appSecret}&js_code=${code}&grant_type=authorization_code`
-    );
+    const https = require('https');
 
-    const data = await wxRes.json();
+    const url = `https://api.weixin.qq.com/sns/jscode2session?appid=${appId}&secret=${appSecret}&js_code=${code}&grant_type=authorization_code`;
+
+    const data = await new Promise((resolve, reject) => {
+      https.get(url, (response) => {
+        let body = '';
+        response.on('data', (chunk) => { body += chunk; });
+        response.on('end', () => {
+          try { resolve(JSON.parse(body)); }
+          catch (e) { reject(e); }
+        });
+      }).on('error', reject);
+    });
 
     if (data.errcode) {
       console.error('WeChat API error:', data);
@@ -49,4 +56,4 @@ export default async function handler(req, res) {
     console.error('Get openid error:', error);
     return res.status(500).json({ error: 'Internal server error' });
   }
-}
+};
